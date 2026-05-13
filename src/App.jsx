1dda36f1e61d06334630878556eb2dae4abce54b
Evelinmq <20243ds170@utez.edu.mx>
2026-05-13 15:50:04 -0600

    Navegación de prueba para el Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
-import './App.css'
+import './App.css';
+
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import Sidebar from "./Components/Sidebar";
+import Beneficiarios from "./Pages/Beneficiarios";
+import Afiliados from "./Pages/Afiliados";
+import Administracion from "./Pages/Administracion";
+import Programas from "./Pages/Programas";
+import Secciones from "./Pages/Secciones";
+import Alianzas from "./Pages/Alianzas";
+import Objetivos from "./Pages/Objetivos.jsx";
 
 function App() {
 
-  return (
-    <>
-    </>
-
-  )
+    return (
+        <BrowserRouter>
+            <Sidebar />
+            <div className="sidebar-page-content">
+                <Routes>
+                    <Route path="/" element={<Navigate to="/programas" />} />
+                    <Route path="/beneficiarios" element={<Beneficiarios />} />
+                    <Route path="/afiliados" element={<Afiliados />} />
+                    <Route path="/administracion" element={<Administracion />} />
+                    <Route path="/programas" element={<Programas />} />
+                    <Route path="/secciones" element={<Secciones />} />
+                    <Route path="/alianzas" element={<Alianzas />} />
+                    <Route path="/objetivos" element={<Objetivos />} />
+                </Routes>
+            </div>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
