522a8d16100c13e0361dc28e70d349c5db974bd3
Andreaara <20243ds171@utez.edu.mx>
2026-05-14 14:27:06 -0600

    Creación de componente Vision y Mision

---
+++ src/Components/Informacion/Vision.jsx
@@ -0,0 +1,14 @@
+
+
+export default function Vision({descripcion }) {
+    return (
+        <div className="card-Vision">
+
+            <div >
+                <h3 className="titulo-Vision-Mision">VISIÓN</h3>
+                <p className="descripcion-Vision-Mision">{descripcion}</p>
+            </div>
+        </div>
+
+    );
+}
